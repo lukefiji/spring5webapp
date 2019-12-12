70c45b8e5f15c960a941854ae1e09d5626cd5490
Luke Fiji <luke.fiji@gmail.com>
2019-12-11 22:08:13 -0800

    23 Add Books Thymeleaf template

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// Indicates that it is a Spring bean
 @Controller
 public class BookController {
 
@@ -15,11 +16,12 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
+    // When a '/books' request comes in
     @RequestMapping("/books")
     public String getBooks(Model model){
 
         // Tell Spring Data JPA to go out and get a list of books from db
-        // Spring MVC will associate list an pass on into view
+        // Spring MVC will associate list and pass on into view
         model.addAttribute("books", bookRepository.findAll());
 
         // Associate with a Thymeleaf view
